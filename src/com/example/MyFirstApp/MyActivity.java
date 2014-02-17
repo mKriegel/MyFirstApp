53cbe4a32e9843642a19aa46d4285c63b2974a0a
Mason Kriegel <polevault25@Mason.local>
2014-02-16 19:49:36 -0600

    Initial commit

---
+++ src/com/example/MyFirstApp/MyActivity.java
@@ -0,0 +1,30 @@
+package com.example.MyFirstApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class MyActivity extends Activity {
+
+    /** Constants **/
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    /** Called when the user clicks the send button **/
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
